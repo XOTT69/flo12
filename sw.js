ef3eb9271dbf0f2face6300ead6f0c4b85fba66b
Антон <mikolenko.anton1@gmail.com>
2026-01-01 13:25:46 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='flo-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','index.html','manifest.json']))));self.addEventListener('fetch',e=>caches.match(e.request).then(r=>r||fetch(e.request)));
